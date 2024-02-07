71bd647c42923683958d4f0083ddac70a07cfd55
g2server <>
2024-02-07 11:30:01 +1000

    Add basic global error catching and logging

--- lib/main.dart
+++ lib/main.dart
@@ -1,3 +1,5 @@
+import 'dart:async';
+
 import 'package:dictionary/app.dart';
 import 'package:dictionary/shared/logging/pretty_logger.dart';
 import 'package:flutter/material.dart';
@@ -7,13 +9,26 @@ import 'package:flutter_riverpod/flutter_riverpod.dart';
 var logger = PrettyLogger();
 
 void main() async {
-  WidgetsFlutterBinding.ensureInitialized();
-  await logger.init();
-  logger.i('logger initialized');
+  runZonedGuarded(() async {
+    WidgetsFlutterBinding.ensureInitialized();
 
-  runApp(
-    const ProviderScope(
-      child: MyApp(),
-    ),
-  );
+    // TODO. consider moving async app init logic out of main and into a "app startup" widget
+    // so errors can be retried.
+    // see https://codewithandrea.com/articles/robust-app-initialization-riverpod/
+    await logger.init();
+    logger.i('logger initialized');
+
+    FlutterError.onError = (details) {
+      FlutterError.presentError(details);
+      logger.e('FlutterError.onError: ${details.exceptionAsString()}');
+    };
+
+    runApp(
+      const ProviderScope(
+        child: MyApp(),
+      ),
+    );
+  }, (error, stackTrace) {
+    logger.e('runZonedGuarded: ${error.toString()}');
+  });
 }
